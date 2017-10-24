52ac0a138d030467d57c8fd46d4734ed8bd4cf8d
Guilherme Brondani Torri <guitorri@gmail.com>
2017-10-24 21:37:39 +0200

    Fix non viable conversion from iterator
    Error: no viable conversion
      from
        __map_iterator<__tree_iterator<__value_type<const basic_string<...>, ...
      to
        __map_iterator<__tree_iterator<__value_type<basic_string<...>, ...
    
    Compiler:
      Apple LLVM version 9.0.0 (clang-900.0.37)
      Target: x86_64-apple-darwin16.7.0

--- lib/c_attach.cc
+++ lib/c_attach.cc
@@ -36,7 +36,7 @@ std::string plug_path()
 /*--------------------------------------------------------------------------*/
 void list()
 {
-  for (std::map<std::string, void*>::iterator
+  for (std::map<const std::string, void*>::iterator
 	 ii = attach_list.begin(); ii != attach_list.end(); ++ii) {
     if (ii->second) {
       IO::mstdout << ii->first << '\n';
@@ -179,7 +179,7 @@ public:
   void do_it(CS& cmd, CARD_LIST*)
   {
     if (CARD_LIST::card_list.is_empty()) {
-      for (std::map<std::string, void*>::iterator
+      for (std::map<const std::string, void*>::iterator
 	     ii = attach_list.begin(); ii != attach_list.end(); ++ii) {
 	void* handle = ii->second;
 	if (handle) {
